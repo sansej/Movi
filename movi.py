c63514dd5d546a8a448e01f68a2f5cbee010c62c
Sansej <sansej5000@gmail.com>
2023-11-27 16:15:17 +0100

    тк

--- movi.py
+++ movi.py
@@ -1,28 +1,37 @@
-from mutagen.mp3 import MP3 
+# import soundfile as sf
+# import scipy.io.wavfile as wav
+# from mutagen.mp3 import MP3 
+# import tempfile
+# import moviepy
+# import speech_recognition as sr
+# from moviepy.video.tools.subtitles import file_to_subtitles
+# from pydub.silence import split_on_silence
+# from googletrans import Translator
+# from moviepy.editor import concatenate_audioclips
+# import vosk
+# import json
+# import wave
+# import sys
+import librosa.display
+import matplotlib.pyplot as plt
+import librosa
+import requests
 from PIL import Image
 import imageio 
 import os 
-import tempfile
-import json
-import vosk
-import wave
-from pydub.silence import split_on_silence
-from googletrans import Translator
-from moviepy.editor import concatenate_audioclips
-from moviepy.editor import TextClip
 from moviepy.video.tools.subtitles import SubtitlesClip
-from moviepy.video.tools.subtitles import file_to_subtitles
 from moviepy.config import change_settings
-import moviepy
-import speech_recognition as sr
 import numpy
 import imageio
 from elevenlabs import generate, save, set_api_key
 import moviepy.editor as mp
-from pydub import AudioSegment, effects
-import soundfile as sf
-import scipy.io.wavfile as wav
-set_api_key(os.environ.get("ELEVEN_KEY"))
+from pydub import AudioSegment
+import pysrt
+from moviepy.editor import VideoFileClip, TextClip, CompositeVideoClip
+
+
+# set_api_key(os.environ.get("ELEVEN_KEY"))
+deepgramapiKey=set_api_key(os.environ.get("DEEPGRAM_API_KEY"))
 
 def crop_image(input_path, output_path, resolution = (720, 1280)):
     original_image = Image.open(input_path).crop()
@@ -119,62 +128,179 @@ def change_speed(input_path, output_path, format = 'wav', speed = 1.07):
     so = sound.speedup(playback_speed = speed)
     so.export(output_path, format = format)
 
-def subtitle_generator():
-    # Замените этот список субтитрами вашими значениями
-    subtitles = [(0, 5, "Привет, это субтитр 1."),
-                 (7, 12, "Это субтитр 2."),
-                 (15, 20, "И это субтитр 3.")]
+# def subtitle_generator():
+#     # Замените этот список субтитрами вашими значениями
+#     subtitles = [(0, 5, "Привет, это субтитр 1."),
+#                  (7, 12, "Это субтитр 2."),
+#                  (15, 20, "И это субтитр 3.")]
+#     change_settings({"IMAGEMAGICK_BINARY": r"C:\\Program Files\\ImageMagick-7.1.1-Q16\\magick.exe"})
+#     generator = lambda txt: TextClip(txt, font='Georgia-Regular', fontsize=60, color='white')
+#     video_clip = mp.VideoFileClip("video\\2_tr.mp4")
+#     subtitles = SubtitlesClip('subtitles.srt', generator)
+#     subtitles = subtitles.set_pos(('center', 'center'))
+#     video_with_subtitles = mp.CompositeVideoClip([video_clip, subtitles])
+#     video_with_subtitles.write_videofile("video\\final_sub.mp4", codec="libx264", audio_codec="aac")
+
+
+# def textclip():
+#     subtitles = [(0, 5, "Привет, это субтитр 1."),
+#                  (7, 12, "Это субтитр 2."),
+#                  (15, 20, "И это субтитр 3.")]
+#     change_settings({"IMAGEMAGICK_BINARY": r"C:\\Program Files\\ImageMagick-7.1.1-Q16\\magick.exe"})
+#     video_clip = mp.VideoFileClip("video\\2_tr.mp4")
+#     text_clip = TextClip('subtitles', fontsize=80, color='white',font='Georgia-Regular')
+#     text_clip = text_clip.set_duration(video_clip.duration)
+#     video_with_text = mp.CompositeVideoClip([video_clip, text_clip.set_pos(('center', 'center'))])
+#     video_with_text.write_videofile("video\\final_sub.mp4", codec="libx264", audio_codec="aac")
+
+# def audio_to_subtitle_v3(audio_file_path, output_subtitle_path, language='ru'):
+#     vosk_model_path = "C:\\Users\sansej\\AppData\\Roaming\\Python\\Python312\\site-packages\\vosk-model-ru-0.42"
+#     vosk_model = vosk.Model(vosk_model_path)
+#     audio = AudioSegment.from_file(audio_file_path)
+#     temp_wav_path = "temp.wav"
+#     audio.export(temp_wav_path, format="wav")
+#     with wave.open(temp_wav_path, 'rb') as wf:
+#         sample_rate = wf.getframerate()
+#         recognizer = vosk.KaldiRecognizer(vosk_model, sample_rate)
+#     with open(temp_wav_path, 'rb') as audio_file:
+#         audio_data = audio_file.read()
+#         recognizer.AcceptWaveform(audio_data)
+#     result = json.loads(recognizer.FinalResult())
+#     recognized_text = result['text']
+#     subtitles = [(0, len(recognized_text.split()), recognized_text)]
+#     srt_content = create_srt(subtitles)
+#     with open(output_subtitle_path, "w", encoding="utf-8") as file:
+#         file.write(srt_content)
+#     os.remove(temp_wav_path)
+
+# def create_srt(subtitles):
+#     srt_content = ""
+#     for i, (start_time, end_time, text) in enumerate(subtitles, start=1):
+#         srt_content += f"{i}\n{format_time(start_time)} --> {format_time(end_time)}\n{text}\n\n"
+#     return srt_content
+
+# def format_time(milliseconds):
+#     seconds, milliseconds = divmod(milliseconds, 1000)
+#     minutes, seconds = divmod(seconds, 60)
+#     hours, minutes = divmod(minutes, 60)
+#     return f"{hours:02d}:{minutes:02d}:{seconds:02d},{milliseconds:03d}"
+
+def convert_to_srt(data, output_filename):
+    def format_time(seconds):
+        # Convert seconds to hours, minutes, seconds, milliseconds format
+        hours, remainder = divmod(seconds, 3600)
+        print(hours, remainder)
+        minutes, remainder = divmod(remainder, 60)
+        print(minutes, remainder)
+        seconds, milliseconds = divmod(remainder, 1)
+        print(seconds, milliseconds)
+        return f"{int(hours):02d}:{int(minutes):02d}:{int(seconds):02d},{int(milliseconds*1000):03d}"
+
+    with open(output_filename, 'w', encoding='UTF-8') as f:
+        for i, entry in enumerate(data, start=1):
+            start_time = format_time(entry['start'])
+            print(start_time)
+            end_time = format_time(entry['end'])
+            print(end_time)
+            subtitle_text = entry['punctuated_word']
+            print(subtitle_text)
+            f.write(f"{i}\n")
+            f.write(f"{start_time} --> {end_time}\n")
+            f.write(f"{subtitle_text}\n\n")
+
+def getDeepgramTranscription(p_url):
+    url = "https://api.deepgram.com/v1/listen?smart_format=true&language=ru&model=whisper-medium"
+    payload = {
+        "url": p_url
+    }
+    headers = {
+        "Authorization": 'Token ' + str(deepgramapiKey),
+        "content-type": "application/json"
+    }
+    response = requests.request("POST", url, headers=headers, json=payload)
+    output = response.json()
+    print(output)
+    return output
+
+def time_to_seconds(time_obj):
+    return time_obj.hours * 3600 + time_obj.minutes * 60 + time_obj.seconds + time_obj.milliseconds / 1000
+
+
+def create_subtitle_clips(subtitles, videosize,fontsize=80, font='Arial', color='yellow', debug = False):
+    subtitle_clips = []
     change_settings({"IMAGEMAGICK_BINARY": r"C:\\Program Files\\ImageMagick-7.1.1-Q16\\magick.exe"})
-    generator = lambda txt: TextClip(txt, font='Georgia-Regular', fontsize=60, color='white')
-    video_clip = mp.VideoFileClip("video\\2_tr.mp4")
-    subtitles = SubtitlesClip('subtitles.srt', generator)
-    subtitles = subtitles.set_pos(('center', 'center'))
-    video_with_subtitles = mp.CompositeVideoClip([video_clip, subtitles])
-    video_with_subtitles.write_videofile("video\\final_sub.mp4", codec="libx264", audio_codec="aac")
+    for subtitle in subtitles:
+        start_time = time_to_seconds(subtitle.start)
+        end_time = time_to_seconds(subtitle.end)
+        duration = end_time - start_time
+        video_width, video_height = videosize
+        text_clip = TextClip(subtitle.text, fontsize=fontsize, font=font, color=color,size=(video_width*3/4, None), method='caption').set_start(start_time).set_duration(duration)
+        subtitle_x_position = 'center'
+        subtitle_y_position = 'center'
+        text_position = (subtitle_x_position, subtitle_y_position)                    
+        subtitle_clips.append(text_clip.set_position(text_position))
+    return subtitle_clips
 
+def mfccs():
+    audio_path = 'out.wav'
+    audio_signal, sampling_rate = librosa.load(audio_path)
+    mfccs = librosa.feature.mfcc(y=audio_signal, sr=sampling_rate)
+    print(mfccs)
 
-def textclip():
-    subtitles = [(0, 5, "Привет, это субтитр 1."),
-                 (7, 12, "Это субтитр 2."),
-                 (15, 20, "И это субтитр 3.")]
-    change_settings({"IMAGEMAGICK_BINARY": r"C:\\Program Files\\ImageMagick-7.1.1-Q16\\magick.exe"})
-    video_clip = mp.VideoFileClip("video\\2_tr.mp4")
-    text_clip = TextClip('subtitles', fontsize=80, color='white',font='Georgia-Regular')
-    text_clip = text_clip.set_duration(video_clip.duration)
-    video_with_text = mp.CompositeVideoClip([video_clip, text_clip.set_pos(('center', 'center'))])
-    video_with_text.write_videofile("video\\final_sub.mp4", codec="libx264", audio_codec="aac")
+# def segment_audio_by_threshold(audio_signal, threshold=0.05, min_segment_length=1000):
+#     """
+#     Сегментация аудиосигнала на отдельные сегменты на основе пороговой обработки.
+    
+#     Параметры:
+#     - audio_signal: массив значений аудиосигнала
+#     - threshold: пороговое значение для определения паузы
+#     - min_segment_length: минимальная длина сегмента, чтобы исключить короткие шумовые периоды
+    
+#     Возвращает:
+#     - segments: список кортежей, представляющих сегменты (начальный индекс, конечный индекс)
+#     """
+#     above_threshold = audio_signal > threshold
+#     segment_starts = numpy.where(numpy.diff(above_threshold.astype(int)) == 1)[0]
+#     segment_ends = numpy.where(numpy.diff(above_threshold.astype(int)) == -1)[0]
 
-def audio_to_subtitle_v3(audio_file_path, output_subtitle_path, language='ru'):
-    vosk_model_path = "C:\\Users\sansej\\AppData\\Roaming\\Python\\Python312\\site-packages\\vosk-model-ru-0.42"
-    vosk_model = vosk.Model(vosk_model_path)
-    audio = AudioSegment.from_file(audio_file_path)
-    temp_wav_path = "temp.wav"
-    audio.export(temp_wav_path, format="wav")
-    with wave.open(temp_wav_path, 'rb') as wf:
-        sample_rate = wf.getframerate()
-        recognizer = vosk.KaldiRecognizer(vosk_model, sample_rate)
-    with open(temp_wav_path, 'rb') as audio_file:
-        audio_data = audio_file.read()
-        recognizer.AcceptWaveform(audio_data)
-    result = json.loads(recognizer.FinalResult())
-    recognized_text = result['text']
-    subtitles = [(0, len(recognized_text.split()), recognized_text)]
-    srt_content = create_srt(subtitles)
-    with open(output_subtitle_path, "w", encoding="utf-8") as file:
-        file.write(srt_content)
-    os.remove(temp_wav_path)
+#     # Удаление коротких сегментов
+#     valid_segments = [(start, end) for start, end in zip(segment_starts, segment_ends) if end - start > min_segment_length]
 
-def create_srt(subtitles):
-    srt_content = ""
-    for i, (start_time, end_time, text) in enumerate(subtitles, start=1):
-        srt_content += f"{i}\n{format_time(start_time)} --> {format_time(end_time)}\n{text}\n\n"
-    return srt_content
+#     return valid_segments
 
-def format_time(milliseconds):
-    seconds, milliseconds = divmod(milliseconds, 1000)
-    minutes, seconds = divmod(seconds, 60)
-    hours, minutes = divmod(minutes, 60)
-    return f"{hours:02d}:{minutes:02d}:{seconds:02d},{milliseconds:03d}"
+def to_normalized_array(audio_chunk, fs, librosa_fs):
+   samples = audio_chunk.get_array_of_samples()
+   arr = numpy.array(samples).astype(numpy.float32) / numpy.iinfo(numpy.int16).max
+   return librosa.core.resample(arr, fs, librosa_fs)
+
+def segment_audio_by_threshold(audio_signal, threshold=0.05, min_segment_length=1000):
+    above_threshold = audio_signal > threshold
+    segment_starts = numpy.where(numpy.diff(above_threshold.astype(int)) == 1)[0]
+    segment_ends = numpy.where(numpy.diff(above_threshold.astype(int)) == -1)[0]
+
+    # Удаление коротких сегментов
+    valid_segments = [(start, end) for start, end in zip(segment_starts, segment_ends) if end - start > min_segment_length]
+
+    return valid_segments
+
+def srt_format_time(segment):
+    start_time = int(segment[0])
+    end_time = int(segment[1])
+
+    start_hours, start_remainder = divmod(start_time, 3600)
+    start_minutes, start_seconds = divmod(start_remainder, 60)
+    start_seconds, start_microseconds = divmod(int((start_seconds - int(start_seconds)) * 1e6), 1e6)
+
+    end_hours, end_remainder = divmod(end_time, 3600)
+    end_minutes, end_seconds = divmod(end_remainder, 60)
+    end_seconds, end_microseconds = divmod(int((end_seconds - int(end_seconds)) * 1e6), 1e6)
+
+    formatted_time = "{:02d}:{:02d}:{:02d},{:06d} - {:02d}:{:02d}:{:02d},{:06d}".format(
+        start_hours, start_minutes, int(start_seconds), int(start_microseconds),
+        end_hours, end_minutes, int(end_seconds), int(end_microseconds)
+    )
+
+    return formatted_time
 
 def main():
     # crop_image(input_path='ntc.jpg', output_path='crop_ntc.jpg')
@@ -195,7 +321,7 @@ def main():
     # change_speed(input_path='out.wav', output_path='final.wav')
     # combinate(video_path='video\\9_tr.mp4', audio_path='final.wav',output_path='video\\final.mp4')
     # subtitle_generator()
-    textclip()
+    # textclip()
     # create_srt()
 
     # Example usage
@@ -231,6 +357,106 @@ def main():
     #         create_transition(clip1_path=f'{folder_video}\\{file_name}', clip2_path=f'{folder_video}\\{file}',output_file=f'{folder_video}\\{count}_tr.mp4',overlap=0.75)
     #         file_name = f'{count}_tr.mp4'
     #     count += 1
+
+    # srtfilename = "sub.srt"
+    # mp4filename = "video_with_voice.mp4"
+    # video = VideoFileClip(mp4filename)
+    # subtitles = pysrt.open(srtfilename)
+    # begin,end= mp4filename.split(".mp4")
+    # output_video_file = begin+'_sub'+".mp4"
+    # print ("Output file name: ",output_video_file)
+    # subtitle_clips = create_subtitle_clips(subtitles,video.size)
+    # final_video = CompositeVideoClip([video] + subtitle_clips)
+    # final_video.write_videofile(output_video_file)
+
+    # mp3url = "https://static.deepgram.com/examples/Bueller-Life-moves-pretty-fast.wav"
+
+    # # print(output)
+    # subtitle_data = output['results']['channels'][0]['alternatives'][0]['words']
+    # filename = os.path.basename(mp3url)
+    # name, extension = os.path.splitext(filename)
+    # output_filename = name + ".srt"
+    # convert_to_srt(subtitle_data,output_filename)
+
+    # mfccs()
+
+    # # Пример использования
+    # audio_path = 'out.wav'
+    # audio_signal, sampling_rate = librosa.load(audio_path)
+    # # Применение пороговой обработки
+    # threshold = 0.1
+    # min_segment_length = 25
+    # segments = segment_audio_by_threshold(audio_signal, threshold, min_segment_length)
+    # # Вывод результатов
+    # for start, end in segments:
+    #     print(f"Сегмент: {start} - {end} (Длительность: {end - start} сэмплов)")
+
+    # audio_chunk = AudioSegment.from_file("out.wav")
+    # audio_chunk = audio_chunk.set_sample_width(2).set_channels(1).set_frame_rate(16000)
+    # fs = 16000
+    # librosa_fs = 22050
+    # top_db = 20
+    # arr = to_normalized_array(audio_chunk, fs, librosa_fs)
+    # edges = librosa.effects.split(arr, top_db=top_db) / librosa_fs
+    # print(edges)
+
+    # audio_file_path = 'out.wav'
+    # y, sr = librosa.load(audio_file_path)
+    # print(f"Частота дискретизации: {sr} Гц")
+
+
+    # audio_path = 'out.wav'
+    # audio_signal, sampling_rate = librosa.load(audio_path)
+    # normalized_audio = librosa.effects.preemphasis(audio_signal)
+    # speed_changed_audio = librosa.effects.time_stretch(normalized_audio, rate=1.5)
+    # # plt.figure(figsize=(12, 8))
+    # # plt.subplot(2, 1, 1)
+    # # librosa.display.waveshow(normalized_audio, sr=sampling_rate)
+    # # plt.title('Исходный аудио сигнал')
+    # # plt.subplot(2, 1, 2)
+    # # librosa.display.waveshow(speed_changed_audio, sr=sampling_rate)
+    # # plt.title('Аудио с измененной скоростью')
+    # # plt.tight_layout()
+    # # plt.show()
+    # threshold = 0.05
+    # min_segment_length = 1000
+    # segments = segment_audio_by_threshold(speed_changed_audio, threshold, min_segment_length)
+    # for start, end in segments:
+    #     print(f"Сегмент: {start} - {end} (Длительность: {end - start} сэмплов)")
+
+    # data_array = [
+    #     [0.16253968, 0.60371882],
+    #     [1.04489796, 1.4860771],
+    #     [1.50929705, 2.78639456],
+    #     [2.83283447, 3.4829932],
+    #     [3.50621315, 3.64553288],
+    #     [3.71519274, 4.01705215],
+    #     [4.06349206, 5.24770975],
+    #     [5.4799093, 5.64244898],
+    #     [5.71210884, 6.01396825],
+    #     [6.08362812, 6.29260771],
+    #     [6.31582766, 7.70902494]
+    # ]
+
+    # # Преобразование массива
+    # formatted_times = [srt_format_time(segment) for segment in data_array]
+
+    # # Вывод результатов
+    # for i, time in enumerate(formatted_times, start=1):
+    #     print("Segment {}: {}".format(i, time))
+
+    value = 57.70902494
+
+    seconds, microseconds = divmod(int(value * 1e6), 1e6)
+    hours, remainder = divmod(seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
+
+    formatted_time = "{:02d}:{:02d}:{:02d},{:06d}".format(
+        int(hours), int(minutes), int(seconds), int(microseconds)
+    )
+
+    print("Formatted time: {}".format(formatted_time))
+
 
 
 if __name__ == "__main__":
